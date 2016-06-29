775446d37139f18d480c4a7146294b260817bae8
Dan Radez <dradez@redhat.com>
2016-06-29 11:33:28 -0400

    moving startup.conf to an erb temlate

--- manifests/vpp/config.pp
+++ manifests/vpp/config.pp
@@ -6,24 +6,8 @@ class fdio::vpp::config (
   $dpdk_pmd_type = $::fdio::params::dpdk_pmd_type,
   $dpdk_pci_devs = $::fdio::params::dpdk_pci_devs,
 ){
-  file_line { 'startup.conf_cli-listen':
-    path  => '/etc/vpp/startup.conf',
-    line  => "    cli-listen localhost:5002",
-    after => '^unix.*$',
-  }
-
-  file_line { 'startup.conf_uio-driver':
-    path  => '/etc/vpp/startup.conf',
-    line  => "    uio-driver ${dpdk_pmd_type}",
-    match => '.*uio-driver.*$',
-  }
-
-  each($dpdk_pci_devs) |$value| {
-    file_line { "startup.conf_dev-$value":
-      path  => '/etc/vpp/startup.conf',
-      line  => "    dev ${value}",
-      after => '^dpdk.*$',
-    }
+  file { '/etc/vpp/startup.conf':
+    content => template('fdio/startup.conf.erb'),
   }
 
   # ensure that uio-pci-generic is loaded
